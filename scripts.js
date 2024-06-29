b9fc82414992dd78a7272f95364011944fe85c16
KHartzer <hartzerkaile@icloud.com>
2024-06-29 14:10:48 +0200

    Added functional script to scripts.js and removed index.html and styles.css

--- scripts.js
+++ scripts.js
@@ -1,5 +1,36 @@
-const message = () => {
-    console.log("Mixed Messages Project");
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num);
 }
 
-message();
+const messages = {
+    joke: ['', '', ''],
+    inspire: ['', '', ''],
+    advice: ['', '', ''],
+}
+
+let personalMessage = [];
+
+for (let prop in messages) {
+    let optionId = generateRandomNumber(messages[prop].length)
+
+    switch(prop) {
+        case 'joke':
+            personalMessage.push(`${messages[prop][optionId]}`)
+            break;
+        case 'inspire':
+            personalMessage.push(`${messages[prop][optionId]}`)
+            break;
+        case 'advice':
+            personalMessage.push(`${messages[prop][optionId]}`)
+            break;
+        default:
+            personalMessage.push(`Sorry, I cannot help you.`)
+    }
+}
+
+function formatMessage(message) {
+    const formatted = personalMessage.join('\n')
+    console.log(formatted)
+}
+
+formatMessage(personalMessage);
